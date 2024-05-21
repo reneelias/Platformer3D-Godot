9e379a706446a05693addc6c2513bf6f7e45f96a
Rene Elias <reneelias03@gmail.com>
2024-05-21 16:46:25 -0700

    Camera smooth lerp follow

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var money = 0
+@export var cameraController : Node3D
+@export var cameraLerpSpeed : float = .1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -19,7 +21,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -30,6 +31,10 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	# Make CameraController match the position of myself
+	cameraController.position = lerp(cameraController.position, position, cameraLerpSpeed)
+	# $CameraController.position = lerp($CameraController.position, position, cameraLerpSpeed)
 
 func addToMoney(amount):
 	money += amount
