712a79c39c7138e29ec35ee89c978f2abc53dbae
Rene Elias <reneelias03@gmail.com>
2024-05-31 13:06:35 -0700

    Arrow keys turn camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = 4.5
 var money = 0
 @export var cameraController : Node3D
 @export var cameraLerpSpeed : float = .1
+@export var camerTurnSpeed : float = 2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -20,8 +21,13 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("movement_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_key_pressed(KEY_LEFT):
+		rotate_y(deg_to_rad(camerTurnSpeed))
+	elif Input.is_key_pressed(KEY_RIGHT):
+		rotate_y(deg_to_rad(-camerTurnSpeed))
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
+	input_dir.rotated(rotation.y)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -34,6 +40,7 @@ func _physics_process(delta):
 
 	# Make CameraController match the position of myself
 	cameraController.position = lerp(cameraController.position, position, cameraLerpSpeed)
+	cameraController.rotation.y = rotation.y
 	# $CameraController.position = lerp($CameraController.position, position, cameraLerpSpeed)
 
 func addToMoney(amount):
